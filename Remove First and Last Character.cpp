43a1bd90cea25b9b702b49ee3fad360b037e3fea
alaa <alaasharshera123@gmail.com>
2024-06-27 07:59:23 +0300

    new solution

--- Remove First and Last Character.cpp
+++ Remove First and Last Character.cpp
@@ -40,9 +40,9 @@ using namespace std;
 
 
 string sliceString(string str) {
-    if (str.length() <= 2) {
-        return "";
-    }
+    // if (str.length() <= 2) {
+    //     return "";
+    // }
     return str.substr(1, str.length() - 2);
 }
 
